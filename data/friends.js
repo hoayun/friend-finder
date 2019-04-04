6b31d9e16c7ad332c40714856689d2db24451549
Chris Hoayun <45009760+hoayun@users.noreply.github.com>
2019-04-03 23:41:34 -0400

    questions class changed

--- data/friends.js
+++ data/friends.js
@@ -1,2 +1,19 @@
-var friends = [];
+var friends = [{
+    name: 'Dennis',
+    photo: 'https://www.thewrap.com/wp-content/uploads/2018/01/Always-Sunny-Dennis-Reynolds.jpg',
+    scores: [
+        '1',
+        '5',
+        '5',
+        '1',
+        '5',
+        '5',
+        '1',
+        '1',
+        '1',
+        '5'
+    ]
+}];
+
+
 module.exports = friends;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,11 +1,11 @@
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 module.exports = function(app) {
     // API GET Requests
     // Below code handles when users "visit" a page.
     // In each of the below cases when a user visits a link
     // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
     // ---------------------------------------------------------------------------
-  
+  console.log(friends);
     app.get("/api/friends", function(req, res) {
       res.json(friends);
     });
